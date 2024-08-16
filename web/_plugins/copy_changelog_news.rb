37f93b2a302aa25c105d5fd3593ddbf799221812
Ellpeck <me@ellpeck.de>
2024-08-16 14:02:15 +0200

    also copy the changelog and in-game news raw files to the site

--- web/_plugins/copy_changelog_news.rb
+++ web/_plugins/copy_changelog_news.rb
@@ -2,8 +2,12 @@
 FileUtils.cp '../InGameNews.md', '_includes/__news.md'
 FileUtils.cp '../Changelog.md', '_includes/__changelog.md'
 
-# after the site is generated, copy changelog and news media into it
 Jekyll::Hooks.register :site, :post_write do
+    # after the site is generated, copy changelog and news media into it
     FileUtils.cp_r "../media/news", "_site/media"
     FileUtils.cp_r "../media/changelog", "_site/media"
+
+    # also copy the raw markdown files for the in-game changelog/news viewers
+    FileUtils.cp_r "_includes/__news.md", "_site/ingamenews/raw.md"
+    FileUtils.cp_r "_includes/__changelog.md", "_site/changelog/raw.md"
 end
